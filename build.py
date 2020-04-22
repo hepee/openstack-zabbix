8ba85535121a97684d98987719efd6077a2f55c9
hepeng <hepeng@example.com>
2020-04-22 13:43:44 +0800

    update

--- build.py
+++ build.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-def read_service_list():
+def read_service_list(service_list):
 
-    with open(r'./service.list', 'r') as f:
-        service_list = [line.rstrip('\n') for line in f]
+    with open(r'./' + service_list, 'r') as f:
+        service = [line.rstrip('\n') for line in f]
         f.close()
-    return service_list
+    return service
 
 def generate_sh(service):
-    f_sh = open(r'./script/' + service + '.sh', 'w')
+    f_sh = open(r'./etc/zabbix/script/' + service + '.sh', 'w')
     f_sh.write('#!/bin/bash\n\n' + service.split('.')[0].replace('-', '_').upper() + "_STATUS='systemctl is-active " + \
                service.split('.')[0] + "'\n\nif [[  $" + service.split('.')[0].replace('-', '_').upper() + ' == "active"   ]]\n' \
                + 'then\n\n' + '    echo 1\n' + 'else\n' + '    echo 0\n\n' + 'fi'
@@ -16,11 +16,13 @@ def generate_sh(service):
     f_sh.close()
 
 def generate_conf(service):
-    f_conf = open(r'./zabbix_agentd.d/' + service + '.conf', 'w')
+    f_conf = open(r'./etc/zabbix/zabbix_agentd.d/' + service + '.conf', 'w')
     f_conf.write('UserParameter=' + service + ',/etc/zabbix/script/' + service + '.sh')
 
 if __name__=="__main__":
-    l = read_service_list()
+    service_list = 'controller-service.list'
+    # service_list = 'compute-service.list'
+    l = read_service_list(service_list)
     for i in l:
         generate_sh(i)
         generate_conf(i)
